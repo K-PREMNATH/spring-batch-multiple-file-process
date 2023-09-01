8660ba94408f7db415acee448796834b5975c681
Premnath <rexprem1991@gmail.com>
2023-09-01 14:04:37 +0800

    project setup added

---
+++ src/main/java/com/springbatchmultiplefileprocess/SpringBatchMultipleFileProcessApplication.java
@@ -0,0 +1,13 @@
+package com.springbatchmultiplefileprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMultipleFileProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchMultipleFileProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbatchmultiplefileprocess/SpringBatchMultipleFileProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatchmultiplefileprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMultipleFileProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
